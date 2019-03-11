25193368fad977002ee9902ceae910c15d2f94c9
Marlon <m.de.la.cruz@hotmail.com>
2019-03-11 23:32:03 +0000

    added download_avatar

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+// console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    // ...
+
+
+
+}
+
+getRepoContributors(repoOwner, repoName, function (err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+});
