23c5c14c7469a2447cbe0cc976be870808998365
Marlon <m.de.la.cruz@hotmail.com>
2019-03-12 00:03:21 +0000

    gitignore updates/ download_avatar

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,25 @@
 var request = require('request');
 
-// console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
     // ...
 
-
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': 'Authorization'
+        }
+    };
 
 }
 
-getRepoContributors(repoOwner, repoName, function (err, result) {
+getRepoContributors("jquery", "jquery", function (err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
 });
+
+
+
+
---
+++ secrets.js
@@ -0,0 +1,4 @@
+module.exports = {
+    GITHUB_TOKEN: 07577b3e69b57cc35520cdbe4629fff32bc75369
+
+};
